514394c1217f654aeb008b10e7d7c4c7f6baef87
sterligova <id.sterligova@gmail.com>
2024-02-08 13:54:39 +0100

    tests_changes

--- test_analysis_births.py
+++ test_analysis_births.py
@@ -1,27 +1,27 @@
-import unittest
 import pandas as pd
+import pytest
 
-class TestBirthsAnalysis(unittest.TestCase):
+@pytest.fixture(scope='class')
+def births_data():
+    return pd.read_csv('births.csv')
+
+class TestBirthsAnalysis:
     
-    @classmethod
-    def setUpClass(cls):
-        cls.births_data = pd.read_csv('births.csv')
-    
-    def test_data_loaded(self):
-        self.assertIsNotNone(self.births_data)
+    def test_data_loaded(self, births_data):
+        assert births_data is not None
         
-    def test_data_columns(self):
+    def test_data_columns(self, births_data):
         expected_columns = ['date', 'births']
-        self.assertEqual(list(self.births_data.columns), expected_columns)
+        assert list(births_data.columns) == expected_columns
         
-    def test_date_column_format(self):
-        self.assertIsInstance(self.births_data['date'].iloc[0], pd.Timestamp)
+    def test_date_column_format(self, births_data):
+        assert isinstance(births_data['date'].iloc[0], pd.Timestamp)
         
-    def test_group_by_year_and_month(self):
-        grouped = self.births_data.groupby([self.births_data['date'].dt.year, self.births_data['date'].dt.month]).sum()
-        self.assertEqual(grouped.shape[0], 12)  # Проверяем, что есть 12 месяцев в результате
+    def test_group_by_year_and_month(self, births_data):
+        grouped = births_data.groupby([births_data['date'].dt.year, births_data['date'].dt.month]).sum()
+        assert grouped.shape[0] == 12  # Проверяем, что есть 12 месяцев в результате
         
     # Другие тесты...
-    
+
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
